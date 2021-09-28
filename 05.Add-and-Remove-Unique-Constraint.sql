c8a6f66e4e57d9b1bd59edbf8ff7cf86fae86933
Peter Karagyozov <pepitoitalianeca@yahoo.com>
2021-09-28 18:39:31 +0300

    Modified

--- 05.Add-and-Remove-Unique-Constraint.sql
+++
@@ -1,9 +0,0 @@
--- 8. Add / Remove Unique Constraint --
-
-SELECT * FROM pets;
-
-ALTER TABLE pets
-ADD CONSTRAINT u_species UNIQUE (species);
-
-ALTER TABLE pets
-DROP INDEX u_species;
--- 06.Modify-Column.sql
+++
@@ -1,9 +0,0 @@
--- 9. Rename Column name --
-SELECT * FROM pets;
-
-ALTER TABLE pets CHANGE `species` `animal_type` VARCHAR(20);
-
--- 10. Change Column Data Type --
-ALTER TABLE addresses MODIFY city VARCHAR(30);
-
-DESCRIBE addresses;
