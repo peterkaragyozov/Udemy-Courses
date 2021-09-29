a6618f87d8ade2459f60d791fcf83c1125325d40
Peter Karagyozov <pepitoitalianeca@yahoo.com>
2021-09-29 03:20:28 +0300

    Added new Where Clauses

--- MySQL-and-Database-Design/03. Selecting From a Table/02.Where-Clause.sql
+++ MySQL-and-Database-Design/03. Selecting From a Table/02.Where-Clause.sql
@@ -25,3 +25,34 @@ WHERE last_name IN ('Taylor', 'Bluth', 'Armstrong');
 
 SELECT * FROM customers
 WHERE first_name NOT IN ('Kathie', 'John', 'George');
+
+-- 6. BETWEEN --
+SELECT product_id, customer_id, order_time FROM orders
+WHERE order_time BETWEEN '2017-01-01' AND '2017-01-07';
+
+SELECT product_id, customer_id, order_time FROM orders
+WHERE customer_id BETWEEN 5 AND 10;
+
+SELECT * FROM customers
+WHERE last_name BETWEEN 'A' and 'L';
+
+-- 7. LIKE --
+-- This will return all last names that start with W
+SELECT * FROM customers
+WHERE last_name LIKE 'W%';
+
+-- This will return all last names that have O somewhere in them
+SELECT * FROM customers
+WHERE last_name LIKE '%O%';
+
+-- This will return all last names that end with W
+SELECT * FROM customers
+WHERE last_name LIKE '%W';
+
+-- This will return all first names that have only one character before O and one character after it
+SELECT * FROM customers
+WHERE first_name LIKE '_O_';
+
+-- This will return all prices that start with 3
+SELECT * FROM products
+WHERE price LIKE '3%';
